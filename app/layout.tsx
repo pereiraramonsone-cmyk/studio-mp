ed908700819b67ccf5ca3f563d65ccd22168f70f
pereiraramonsone-cmyk <pereiraramonsone@gmail.com>
2026-09-12 18:37:55 -0300

    Refactor RootLayout component to use 'any' type

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,3 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="pt-BR">
-      <body style={{margin:0}}>{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }: any) {
+  return <html lang="pt-BR"><body style={{margin:0}}>{children}</body></html>
 }
