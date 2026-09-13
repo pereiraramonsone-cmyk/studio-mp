9784027dfed6455d56a484b2d7cffb7fbe000ce6
pereiraramonsone-cmyk <pereiraramonsone@gmail.com>
2026-09-12 21:58:50 -0300

    Refactor page.js styles and content structure

--- app/page.js
+++ app/page.js
@@ -3,47 +3,92 @@ return(
 <main style={{
 background:"#0a0a0a",
 color:"#fff",
-padding:"24px",
-textAlign:"center"
+padding:"20px",
+textAlign:"center",
+fontFamily:"Arial"
 }}>
 <h1 style={{
 color:"#d4af37",
-fontSize:"32px"
+fontSize:"36px",
+margin:"0"
 }}>
 STUDIO MP
 </h1>
-<p>NOVO HAMBURGO - RS</p>
+<p style={{
+letterSpacing:"3px",
+fontSize:"12px"
+}}>
+NOVO HAMBURGO - RS
+</p>
+
+<img src="https://raw.githubusercontent.com/pereiraramonsone-cmyk/studio-mp/main/app/IMG-20260912-WA0023.jpg"
+style={{
+width:"100%",
+borderRadius:"16px",
+marginTop:"20px"
+}}/>
+
 <div style={{
 background:"#1a1a1a",
-padding:"16px",
-borderRadius:"12px",
-marginTop:"20px"
+padding:"20px",
+borderRadius:"16px",
+marginTop:"20px",
+textAlign:"left"
 }}>
-<h2 style={{color:"#d4af37"}}>
+<h2 style={{
+color:"#d4af37",
+textAlign:"center"
+}}>
 TABELA DE PRECOS
 </h2>
-<p>CORTE SOCIAL R$35</p>
-<p>NA MAQUINA R$25</p>
-<p>NA TESOURA R$40</p>
-<p>BARBA R$25</p>
-<p>BARBOTERAPIA R$35</p>
-<p>SOBRANCELHA R$15</p>
-<p>NARIZ R$20</p>
-<p>ORELHA R$20</p>
+<p>CORTE SOCIAL - R$35</p>
+<p>NA MAQUINA - R$25</p>
+<p>NA TESOURA - R$40</p>
+<p>BARBA - R$25</p>
+<p>BARBOTERAPIA - R$35</p>
+<p>SOBRANCELHA - R$15</p>
+<p>NARIZ - R$20</p>
+<p>ORELHA - R$20</p>
+<p>LUZES - R$90</p>
+<p>PLATINADO - R$120</p>
 </div>
-<a href="https://wa.me/5551999999999"
+
+<img src="https://raw.githubusercontent.com/pereiraramonsone-cmyk/studio-mp/main/app/IMG-20260912-WA0024.jpg"
+style={{
+width:"100%",
+borderRadius:"16px",
+marginTop:"20px"
+}}/>
+
+<img src="https://raw.githubusercontent.com/pereiraramonsone-cmyk/studio-mp/main/app/IMG-20260912-WA0025.jpg"
+style={{
+width:"100%",
+borderRadius:"16px",
+marginTop:"12px"
+}}/>
+
+<a href="https://wa.me/5551999999999?text=Quero%20agendar%20no%20STUDIO%20MP"
 style={{
 display:"block",
 background:"#d4af37",
 color:"#000",
-padding:"16px",
-borderRadius:"10px",
-marginTop:"20px",
+padding:"18px",
+borderRadius:"12px",
+marginTop:"25px",
 fontWeight:"bold",
-textDecoration:"none"
+textDecoration:"none",
+fontSize:"18px"
 }}>
 AGENDAR NO WHATSAPP
 </a>
+
+<p style={{
+marginTop:"20px",
+fontSize:"12px",
+color:"#888"
+}}>
+R. Bartolomeu de Gusmao, 1117 - Canudos
+</p>
 </main>
 )
 }
